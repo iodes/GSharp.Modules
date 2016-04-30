71eda41cfb19d75b7c5912b36489fc78aae07aeb
SO HYUN SEOP <iodesme@gmail.com>
2016-04-30 13:23:08 +0900

    파일 탐색기 및 폴더 탐색기 추가

---
+++ GSharp.Modules.System.Dialog/GFolderBrowserDialog.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+using GSharp.Extension.Abstracts;
+using GSharp.Extension.Attributes;
+
+namespace GSharp.Modules.System.Dialog
+{
+    public class GFolderBrowserDialog : GModule
+    {
+        [GCommand("선택기로 선택된 폴더")]
+        public static string ShowDialog
+        {
+            get
+            {
+                FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return folderDialog.SelectedPath;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ GSharp.Modules.System.Dialog/GOpenFileDialog.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+using GSharp.Extension.Abstracts;
+using GSharp.Extension.Attributes;
+
+namespace GSharp.Modules.System.Dialog
+{
+    public class GOpenFileDialog : GModule
+    {
+        [GCommand("선택기로 선택된 파일")]
+        public static string ShowDialog
+        {
+            get
+            {
+                OpenFileDialog fileDialog = new OpenFileDialog();
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return fileDialog.FileName;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
