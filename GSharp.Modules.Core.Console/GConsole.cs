d04dad22090aa6c7c1cab5a5248ab855e324e604
SO HYUN SEOP <iodesme@gmail.com>
2016-05-15 22:51:22 +0900

    입출력 콘솔 모듈 추가

---
+++ GSharp.Modules.Core.Console/GConsole.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using GSharp.Extension.Abstracts;
+using GSharp.Extension.Attributes;
+
+namespace GSharp.Modules.Core.Console
+{
+    public class GConsole : GModule
+    {
+        #region 객체
+        private static GConsoleUI consoleUI;
+        private static Window consoleWindow;
+        #endregion
+
+        #region 속성
+        [GCommand("콘솔에 입력된 문자")]
+        public static string ReceivedText { get; set; }
+        #endregion
+
+        #region 이벤트
+        [GCommand("콘솔에 입력된 경우")]
+        public static event ReceivedEventHandler Received;
+        public delegate void ReceivedEventHandler();
+        #endregion
+
+        [GCommand("콘솔에 {0} 출력")]
+        public static void Write(string value)
+        {
+            consoleUI.Write(value);
+        }
+
+        [GCommand("입출력 콘솔 열기")]
+        public static void Show()
+        {
+            consoleUI = new GConsoleUI();
+            consoleUI.Received += () =>
+            {
+                ReceivedText = consoleUI.ReceivedText;
+                Received?.Invoke();
+            };
+
+            consoleWindow = new Window
+            {
+                Width = 500,
+                Height = 500,
+                Title = "입출력 콘솔",
+                Content = consoleUI,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+            };
+
+            consoleWindow.Show();
+        }
+
+        [GCommand("입출력 콘솔 닫기")]
+        public static void Close()
+        {
+            consoleWindow?.Close();
+        }
+    }
+}
---
+++ GSharp.Modules.Core.Console/GConsoleUI.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GSharp.Modules.Core.Console
+{
+    /// <summary>
+    /// GConsoleUI.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class GConsoleUI : UserControl
+    {
+        #region 객체
+        private ScrollViewer currentScrollViewer;
+        #endregion
+
+        #region 속성
+        public string ReceivedText { get; set; }
+        #endregion
+
+        #region 이벤트
+        public event ReceivedEventHandler Received;
+        public delegate void ReceivedEventHandler();
+        #endregion
+
+        #region 생성자
+        public GConsoleUI()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region 사용자 함수
+        public void Write(string value)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                listConsole.Items.Add(value);
+
+                if (VisualTreeHelper.GetChildrenCount(listConsole) > 0)
+                {
+                    if (currentScrollViewer == null)
+                    {
+                        Border border = (Border)VisualTreeHelper.GetChild(listConsole, 0);
+                        currentScrollViewer = (ScrollViewer)VisualTreeHelper.GetChild(border, 0);
+                    }
+
+                    currentScrollViewer?.ScrollToBottom();
+                }
+            });
+        }
+        #endregion
+
+        #region 텍스트 입력 이벤트
+        private void textInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (e.Key == Key.Enter)
+                {
+                    ReceivedText = textInput.Text;
+                    listConsole.Items.Add(ReceivedText);
+                    textInput.Clear();
+                    Received?.Invoke();
+                }
+            });
+        }
+        #endregion
+    }
+}
