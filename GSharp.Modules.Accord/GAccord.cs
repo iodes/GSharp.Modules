db2223aa4a919f0dce573ebecd428a4b02bfd8b3
SO HYUN SEOP <iodesme@gmail.com>
2017-06-18 05:56:27 +0900

    모듈 번역의 지역 오류 수정

--- GSharp.Modules.Accord/GAccord.cs
+++ GSharp.Modules.Accord/GAccord.cs
@@ -9,15 +9,15 @@ namespace GSharp.Modules.Accord
 {
     public class GAccord : GModule
     {
-        [GCommand(isTranslated:true)]
-        [GTranslation("{0}파일 열기", Locale.JA_JP)]
+        [GCommand(isTranslated: true)]
+        [GTranslation("{0}파일 열기", Locale.KO_KR)]
         [GTranslation("{0}ファイルを開く", Locale.JA_JP)]
         public static DataTable Load(string path)
         {
             return new CsvReader(path, true).ToTable();
         }
 
-        [GCommand(isTranslated:true)]
+        [GCommand(isTranslated: true)]
         [GTranslation("{0}의 모든 공백 정리", Locale.KO_KR)]
         [GTranslation("{0}のすべてのスペースを削除", Locale.JA_JP)]
         public static void ClearNullDatas(DataTable data)
