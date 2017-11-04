cdfde8276056da93d69b950fe38888dd28db8550
Minwon Seo <pypygeek@gmail.com>
2017-11-04 23:58:58 +0900

    웹 접속, cpu와 ram 이용률, Ipv4 모듈

---
+++ Gsharp.Moudles.Ipv4/IPv4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using GSharp.Extension.Abstracts;
+using GSharp.Extension.Attributes;
+
+namespace Gsharp.Moudles.Ipv4
+{
+    public class IPv4 : GModule
+    {
+        [GCommand("IPv4 주소")]
+        public static string ShowIPv4()
+        {
+            IPHostEntry host = Dns.GetHostByName(Dns.GetHostName());
+            string ipv4 = host.AddressList[0].ToString();
+
+            return ipv4;
+        }
+    }
+}
