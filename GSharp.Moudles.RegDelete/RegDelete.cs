34281689c42d43a9f60e2d0fdacd240f098e5479
Minwon Seo <pypygeek@gmail.com>
2017-11-06 21:30:43 +0900

    레지스트리 하위키 삭제 모듈

---
+++ GSharp.Moudles.RegDelete/RegDelete.cs
@@ -0,0 +1,19 @@
+﻿using GSharp.Extension.Abstracts;
+using GSharp.Extension.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSharp.Moudles.RegDelete
+{
+    public class RegDelete : GModule
+    {
+        [GCommand("{0}레지스트리 하위키 제거")]
+        public static void RegRemove(string RegPath)
+        {
+            Microsoft.Win32.Registry.LocalMachine.DeleteSubKey(RegPath, false);
+        }
+    }
+}
